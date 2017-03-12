7830018fe9b740296f89dc577a77df1d731b39e3
dongly <xhnszdm@aliyun.com>
2017-03-13 07:39:30 +0800

    1

---
+++ taotao-manager/taotao-manager-web/src/test/java/com/taotao/mapper/AbcTest.java
@@ -0,0 +1,17 @@
+package com.taotao.mapper;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by dongly on 17-3-12.
+ */
+public class AbcTest {
+
+    @Test
+    public void test() {
+
+    }
+
+}
